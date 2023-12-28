924ebe087d26f2af44d0326a4c84130be8dd1290
Artyom-Khachatryan <xachatryanartyom17@gmail.com>
2023-12-28 12:49:21 +0000

    adding util.py changes

--- util.py
+++ util.py
@@ -19,12 +19,20 @@ class Seq2SQLDataset(Dataset):
                 self.list_dataset.append(i)
         elif self.dataset_name =='spider':
             assert self.split!='test', "Seq2SQL model only have train and validation datasets"
-            self.dataset = load_dataset('spider', split = self.split)
+            if self.split == 'train':
+                l = list(range(3629, 3711))
+                self.dataset = load_dataset('spider', split = self.split)
+                self.dataset = self.dataset.select(
+                (i for i in range(len(self.dataset))
+                if i not in l)
+                )
+            else:
+                self.dataset = load_dataset('spider', split = self.split)
             self.list_dataset = []
             for i in self.dataset:
                 if self.split == 'train':
                     conn = sqlite3.connect(f"spid/database/{i['db_id']}/{i['db_id']}.sqlite")
-                if self.split == 'validation':
+                else:
                     conn = sqlite3.connect(f"spid/test_database/{i['db_id']}/{i['db_id']}.sqlite")
 
                 cursor = conn.cursor()
@@ -81,24 +89,17 @@ class Seq2SQLDataset(Dataset):
             question = self.dataset[index]['question']
             sql = self.dataset[index]['query']
 
-            if self.split == 'train':
-                instruction = f"I have following SQL database: {self.list_dataset[index]['db_id']}s. Wher I have list of dictionarys, each of them I have two keys 'table' and 'columns'(table and corresponding columns): {self.list_dataset[index]['table']}.Generate SQL query for this question: {question}"
-                input_text = instruction + '</pet>' + sql + '</s>'
-                input_ids = self.tokenizer(input_text, max_length = self.max_length, padding="max_length", return_tensors = 'pt', truncation = True)['input_ids'].squeeze(0)
-            
-                labels = input_ids.clone()
-                pet_where = self.tokenizer.encode('</pet>')[1]
-                pet_index = int(torch.where(labels==pet_where)[0])
-                labels[0: pet_index+1] = self.ignore_id
-                labels[labels==self.tokenizer.pad_token_id]= self.ignore_id
-                a = {'input_ids': input_ids, 'labels': labels}
-                return a
-            else:
-                question += '</pet>'
-                input_ids= self.tokenizer(question, return_tensors = 'pt', truncation = True)['input_ids'].squeeze(0)
-                labels = self.tokenizer(sql, return_tensors = 'pt')['input_ids'].squeeze(0)[1: ]
-                a = {'input_ids': input_ids, 'labels': labels}
-                return a
+            instruction = f"I have following SQL database: {self.list_dataset[index]['db_id']}s. Wher I have list of dictionarys, each of them I have two keys 'table' and 'columns'(table and corresponding columns): {self.list_dataset[index]['table']}.Generate SQL query for this question: {question}"
+            input_text = instruction + '</pet>' + sql + '</s>'
+            input_ids = self.tokenizer(input_text, max_length = self.max_length, padding="max_length", return_tensors = 'pt', truncation = True)['input_ids'].squeeze(0)
+        
+            labels = input_ids.clone()
+            pet_where = self.tokenizer.encode('</pet>')[1]
+            pet_index = int(torch.where(labels==pet_where)[0])
+            labels[0: pet_index+1] = self.ignore_id
+            labels[labels==self.tokenizer.pad_token_id]= self.ignore_id
+            a = {'input_ids': input_ids, 'labels': labels}
+            return a
             
     def table(self, index):
         if self.dataset_name == 'wikisql':
